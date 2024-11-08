01141dfce783fab622d0e8f6bf6703fdcdade110
Fabio Bilac <fabiobilac@gmail.com>
2024-11-07 21:43:04 -0300

    arrumando o arquivo script.sh

--- script.sh
+++ script.sh
@@ -7,16 +7,6 @@ sudo systemctl start docker
 
 sudo docker pull 
 
-#!/bin/bash
-
-sudo apt-get update
-
-sudo apt-get install python3 -y
-sudo apt-get install curl git zip -y
-sudo apt-get install docker.io -y
-sudo systemctl enable --now docker
-sudo systemctl start docker
-
 sleep 20s
 
 sudo docker pull bilac88/demoday-dvp-202403
